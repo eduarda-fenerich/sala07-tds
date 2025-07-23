80ffae76a49d6436d7c68423b3303b32b5c8dffa
eduarda-fenerich <fenericheduarda13@gmail.com>
2025-07-23 08:26:51 -0300

    Add files via upload

---
+++ exercício01/script.js
@@ -0,0 +1,29 @@
+function soma(){
+    var numero1 = document.getElementById("n1").valueAsNumber
+    var numero2 = document.getElementById("n2").valueAsNumber
+    var resposta = document.getElementById("respSoma")
+     //console.log(numero1, numero2)
+     resposta.textContent = numero1 + numero2
+}
+
+function sub(){
+    var numero3 = parseFloat(document.getElementById("n3").value)
+    var numero4 = parseFloat(document.getElementById("n4").value)
+    var resposta = document.getElementById("respSub")
+    resposta.textContent = numero3 - numero4
+
+}
+
+function multi(){
+    var numero5 = parseFloat(document.getElementById("n5").value)
+    var numero6 = parseFloat(document.getElementById("n6").value)
+    var resposta = document.getElementById("respMulti")
+    resposta.textContent = numero5 * numero6
+}
+    
+function div(){
+    var numero7 = parseFloat(document.getElementById("n7").value)
+    var numero8 = parseFloat(document.getElementById("n8").value)
+    var resposta = document.getElementById("respDiv")
+    resposta.textContent = numero7 / numero8
+}
